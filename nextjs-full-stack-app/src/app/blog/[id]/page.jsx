6ff00f807feb3373c263fa3cd31a8dda0c0bd0a6
Leila Bekturgan <leila.bekturgan@gmail.com>
2023-06-11 16:42:18 +0600

    123

--- nextjs-full-stack-app/src/app/blog/[id]/page.jsx
+++ nextjs-full-stack-app/src/app/blog/[id]/page.jsx
@@ -1,53 +1,67 @@
-import React from 'react'
-import styles from './page.module.css'
-import Image from 'next/image'
-import {notFound} from 'next/navigation'
+import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
 async function getData(id) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
     cache: "no-store",
-  })
+  });
+
   if (!res.ok) {
     return notFound()
   }
+
   return res.json();
 }
 
-const BlogPost = async ({params}) => {
-  const data = await getData(params.id) 
+
+export async function generateMetadata({ params }) {
+
+  const post = await getData(params.id)
+  return {
+    title: post.title,
+    description: post.desc,
+  };
+}
+
+const BlogPost = async ({ params }) => {
+  const data = await getData(params.id);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quo hic animi id laudantium dicta et ipsa distinctio, tempora culpa commodi non veritatis natus repellendus doloremque ducimus? Aspernatur distinctio illum aut, reiciendis dignissimos ex!</p>
+          <p className={styles.desc}>
+            {data.desc}
+          </p>
           <div className={styles.author}>
             <Image
-              src="https://images.pexels.com/photos/14941557/pexels-photo-14941557.jpeg?auto=compress&cs=tinysrgb&w=400"
-              alt=''
+              src={data.img}
+              alt=""
               width={40}
               height={40}
               className={styles.avatar}
-            ></Image>
-            <span className={styles.username}>Tom Tom</span>
+            />
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
           <Image
-            src="https://images.pexels.com/photos/17133050/pexels-photo-17133050.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=''
+            src={data.img}
+            alt=""
             fill={true}
             className={styles.image}
-          ></Image>
+          />
         </div>
       </div>
       <div className={styles.content}>
         <p className={styles.text}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsam doloremque doloribus libero maxime, reiciendis qui neque possimus laudantium rem, magnam quo quaerat! Unde soluta corporis dolor saepe voluptatem? Voluptate accusantium odio maiores doloremque impedit sit voluptates molestiae dicta, eos facere ipsam rerum pariatur odit aperiam minima maxime aut laborum. Facere minus quaerat laudantium perferendis quidem eligendi est quia nihil iure. Officia debitis ab incidunt quaerat perferendis illo, iusto praesentium mollitia corrupti quibusdam dicta, inventore cum voluptatem pariatur, sint deleniti rem similique ea facere eligendi tempora voluptatum. Eius, distinctio possimus fugiat accusamus, ex numquam deserunt praesentium error illo totam vitae!
+         {data.content}
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogPost
+export default BlogPost;
--- nextjs-full-stack-app/src/app/portfolio/[category]/page.jsx
+++ nextjs-full-stack-app/src/app/portfolio/[category]/page.jsx
@@ -2,21 +2,42 @@ import React from 'react'
 import styles from './page.module.css'
 import Button from '@/components/Button/Button'
 import Image from 'next/image'
+import {items} from './data.js'
+import { notFound } from 'next/navigation'
+
+const getData = (cat)=>{
+  const data = items[cat]
+
+  if(data){
+    return data
+  }
+  return notFound()
+
+
+}
+
 const Category = ({params}) => {
   // console.log(params)
+  const data = getData(params.category)
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
-          <Button text="See More" url="#"></Button>
+      {data.map(item=>(
+        <div className={styles.item} key={item.id}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+            <Button text="See More" url="#"></Button>
+          </div>
+          <div className={styles.imgContainer}>
+            <Image 
+              className={styles.img} 
+              fill={true} 
+              src={item.image}
+            />
+          </div>
         </div>
-        <div className={styles.imgContainer}>
-          <Image className={styles.img} fill={true} src='https://images.pexels.com/photos/17137556/pexels-photo-17137556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></Image>
-        </div>
-      </div>
+      ))}
     </div>
   )
 }
